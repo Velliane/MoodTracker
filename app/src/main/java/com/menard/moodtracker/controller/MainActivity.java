5940de78d5c8ce46ac48ff4d9245c83d45e6bc53
Emmanuelle <emmanuelle.em.menard@gmail.com>
2019-05-09 20:28:08 +0200

    Modify ViewPager

--- app/src/main/java/com/menard/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/menard/moodtracker/controller/MainActivity.java
@@ -1,47 +1,36 @@
 package com.menard.moodtracker.controller;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v4.view.PagerAdapter;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+
 
 import com.menard.moodtracker.R;
 import com.menard.moodtracker.adapter.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, GestureDetector.OnGestureListener {
 
-    /** View RelativeLayout */
-    private RelativeLayout mRelativeLayout;
     /** Button add comments */
     private ImageButton mBtnAddComments;
     /** Button Show History */
     private ImageButton mBtnShowHistory;
-
-    private int[] imagesPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRelativeLayout = findViewById(R.id.activity_main_layout);
         mBtnAddComments = findViewById(R.id.activity_main_comments_btn);
         mBtnAddComments.setOnClickListener(this);
         mBtnShowHistory = findViewById(R.id.activity_main_history_btn);
         mBtnShowHistory.setOnClickListener(this);
 
-        imagesPager = new int[]{R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
+        int[] imagesPager = new int[]{R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
         // Instantiate ViewPager and set Adapter
         ViewPager pager = findViewById(R.id.activity_main_viewpager);
         pager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager), imagesPager));
